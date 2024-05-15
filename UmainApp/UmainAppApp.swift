9116d22627042c0d7f764a6f93ad3b3602cb5d6e
Vincent <35411050+avipami@users.noreply.github.com>
2024-05-15 17:51:48 +0200

    Initial Commit

---
+++ UmainApp/UmainAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UmainAppApp.swift
+//  UmainApp
+//
+//  Created by Vincent Palma on 2024-05-15.
+//
+
+import SwiftUI
+
+@main
+struct UmainAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
